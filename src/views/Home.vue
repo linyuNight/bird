f986fd0017258a58bec0019b5b6080dd31c73b8b
laurent.lin <laurent.lin@aswatson.com>
2022-01-10 16:36:36 +0800

    提交代码

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home" v-if="$route.query.aaa === '123'">
+    <div>姓名：林煜</div>
+    <div>性别：男</div>
+    <div>年龄：31岁</div>
+    <div>职业：程序员</div>
+    <div>学历：本科</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      isShow: false
+    }
+  },
+  created() {
+    console.log(this.$route)
+  }
+}
+</script>
